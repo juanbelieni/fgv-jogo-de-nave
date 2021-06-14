b32d2dc38b6c9e275611ab65a34e7371faaee0c8
Pedro <pedro.fontourasouza@gmail.com>
2021-06-13 21:39:26 -0300

    Classe criada

--- src/objects/object.py
+++ src/objects/object.py
@@ -8,6 +8,14 @@ class Object:
         self.size = Vector2(width, height)
 
     def collides_with(self, other):
-        pass
+        si = self.pos
+        sf = self.pos + self.size
+        oi = other.pos
+        of = other.pos + other.size
+
+        if si[1] > of[1] or sf[1] < oi[1] or si[0] > of[0] or sf[0] < oi[0]:
+            return False
+        return True
+
     def move(self, delta) -> None:
-        pass
+        self.pos = self.pos + delta
